44d2565a9564817bb739c199953af173daba33b0
ernestorsp <rmv5pqbbjv@privaterelay.appleid.com>
2026-09-02 15:23:06 -0400

    Export direct HOME rescue summary

--- firebase/functions/main.js
+++ firebase/functions/main.js
@@ -1,7 +1,8 @@
 const core = require('./index.js');
 const email = require('./email-functions.js');
 const homeLog = require('./home-log-sync.js');
+const homeRescue = require('./home-rescue-summary.js');
 const consolidate = require('./consolidate-functions.js');
 const bonusSchedule = require('./bonus-schedule-functions.js');
 const bonusReport = require('./bonus-report-functions.js');
-module.exports = {...core, ...email, ...homeLog, ...consolidate, ...bonusSchedule, ...bonusReport};
+module.exports = {...core, ...email, ...homeLog, ...homeRescue, ...consolidate, ...bonusSchedule, ...bonusReport};
